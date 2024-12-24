90060a581ec71a173b50f4aede4825159a99bde9
Richard-arthub <rikshuter@gmail.com>
2024-12-24 04:09:23 +0300

    expression modifying method done

---
+++ src/main/java/org/example/ExpressionCalculator.java
@@ -0,0 +1,187 @@
+package org.example;
+
+import org.example.InvalidExpressionException;
+import org.example.stack;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class ExpressionCalculator {
+
+    private String expression;
+    public String getExpression() {return expression;}
+
+    private boolean isExpressionValid() {
+        boolean isValid = false;
+        // внутри скобок пусто
+        // скобка не имеет пары
+        // не цифра и не оператор
+        // оператор в начале, проверить отдельно
+        // вокруг оператора есть цифры
+        //
+        //
+        return isValid;
+    }
+
+    private char [] putElemIntoArray (char c, int idx, char [] arr)
+    {
+        char [] newArr = new char [arr.length+1];
+        if (idx > 0) System.arraycopy(arr, 0, newArr, 0, idx);
+        newArr[idx] = c;
+        if (arr.length - idx > 0) System.arraycopy(arr, idx, newArr, idx+1, arr.length - idx);
+
+        return newArr;
+    }
+
+    private void modify() { //добавить скобки вокруг всех выражений, определяя верный порядок
+        char [] arrayExpression = expression.toCharArray();
+        char [] opersArray = {'*', '/', '+', '-'};
+
+        //отдельно проверка и добавление скобок в начало-конец
+        if (arrayExpression[0] != '(')
+        {
+            arrayExpression = putElemIntoArray('(', 0, arrayExpression);
+            arrayExpression = putElemIntoArray(')', arrayExpression.length, arrayExpression);
+        }
+        else
+        {
+            boolean areMainBrackets = true;
+            int check = 0;
+            for (int i = 1; i < arrayExpression.length - 1; i++)
+            {
+                if (arrayExpression[i] == '(') check++;
+                else if (arrayExpression[i] == ')') check--;
+                if (check < 0) {
+                    areMainBrackets = false;
+                    break;
+                }
+            }
+            if (!areMainBrackets)
+            {
+                arrayExpression = putElemIntoArray('(', 0, arrayExpression);
+                arrayExpression = putElemIntoArray(')', arrayExpression.length, arrayExpression);
+            }
+        }
+
+        int j = 0;
+        stack<Integer> countOpersInBrackets = new stack<>();
+        stack<Integer> openBracketIndex = new stack<>();
+        stack<Integer> closeBracketIndex = new stack<>();
+        while (j < arrayExpression.length)
+        {
+            if (arrayExpression[j] == '(') {
+                countOpersInBrackets.push(1);
+                openBracketIndex.push(j);
+            }
+            if (operators.contains(arrayExpression[j])) countOpersInBrackets.push(countOpersInBrackets.pop() + 1);
+
+            if (arrayExpression[j] == ')')
+            {
+                closeBracketIndex.push(j);
+                if (countOpersInBrackets.peek() != 1)
+                {
+                    for (int operIter = 0; operIter < 4; operIter++)
+                    {
+                        int l = openBracketIndex.peek();
+                        while (l < closeBracketIndex.peek())
+                        {
+                            if (arrayExpression[l] == opersArray[operIter])
+                            {
+                                if (Character.isDigit(arrayExpression[l-1]))
+                                {
+                                    int i = l-2;
+                                    while (Character.isDigit(arrayExpression[i])) i--;
+                                    arrayExpression = putElemIntoArray('(', i, arrayExpression);
+                                    closeBracketIndex.push(closeBracketIndex.pop()+1);
+                                }
+                                else
+                                {
+                                    int i = l-2;
+                                    int bracketsPair = 1;
+                                    while (bracketsPair > 0 && i > openBracketIndex.peek())
+                                    {
+                                        if (arrayExpression[i] == '(') bracketsPair--;
+                                        if (arrayExpression[i] == ')') bracketsPair++;
+                                        i--;
+                                    }
+                                    arrayExpression = putElemIntoArray('(', i, arrayExpression);
+                                    closeBracketIndex.push(closeBracketIndex.pop()+1);
+                                    l++;
+                                }
+
+                                if (Character.isDigit(arrayExpression[l+1]))
+                                {
+                                    int i = l+2;
+                                    while (Character.isDigit(arrayExpression[i])) i++;
+                                    arrayExpression = putElemIntoArray(')', i, arrayExpression);
+                                    closeBracketIndex.push(closeBracketIndex.pop()+1);
+                                }
+                                else
+                                {
+                                    int i = l+2;
+                                    int bracketsPair = 1;
+                                    while (bracketsPair > 0 && i < closeBracketIndex.peek())
+                                    {
+                                        if (arrayExpression[i] == '(') bracketsPair++;
+                                        if (arrayExpression[i] == ')') bracketsPair--;
+                                        i++;
+                                    }
+                                    arrayExpression = putElemIntoArray(')', i, arrayExpression);
+                                    closeBracketIndex.push(closeBracketIndex.pop()+1);
+                                }
+
+                                countOpersInBrackets.push(countOpersInBrackets.peek() - 1);
+                            }
+                            l++;
+                        }
+                    }
+                }
+            }
+
+            j++;
+        }
+
+
+        expression = String.valueOf(arrayExpression);
+    }
+
+    public void setExpression(String _expression) {
+        expression = _expression;
+        if (!isExpressionValid()) throw new InvalidExpressionException("Некорректный ввод выражения.");
+    }
+
+    private double performCalculation(double num1, double num2, char operator) {
+        // подсчёт через case
+        return 0.;
+    }
+
+
+    static final private List<Character> operators = Arrays.asList('*', '/', '+', '-');
+
+    public double calculate() {
+
+        stack<Double> numStack = new stack<>();
+        stack<Character> symbStack = new stack<>();
+
+        modify();
+
+        char [] charExpr = expression.toCharArray();
+        char currChar, prevChar;
+
+        numStack.push(0.);
+        symbStack.push('('); //первый символ всегда открывающая скобка
+        for (int i = 1; i < expression.length(); i ++) // i = 1 т.к. алгоритм проверяет предыдущий символ на цифру
+        {
+            currChar = charExpr[i];
+            prevChar = charExpr[i-1];
+            if (Character.isDigit(currChar) && Character.isDigit(prevChar)) numStack.push(10. * numStack.peek() + (double)(currChar - '0'));
+            else if (Character.isDigit(currChar)) numStack.push((double)(currChar - '0'));
+            else if (operators.contains(currChar) || currChar == '(') symbStack.push(currChar);
+            else // тогда это закрывающая скобка
+                numStack.push(performCalculation(numStack.peek(), numStack.peek(), symbStack.peek()));
+        }
+
+        return numStack.peek();
+    }
+}
---
+++ src/main/java/org/example/stack.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class stack<T> {
+
+    private class node {
+        T data;
+        node next;
+
+        node(T _data) {
+            this.data = _data;
+            this.next = null;
+        }
+
+        node(T _data, node _next) {
+            this.data = _data;
+            this.next = _next;
+        }
+    }
+
+    private node head;
+
+    public stack() { head = null; }
+
+    public boolean isEmpty() { return head == null; }
+
+    public void push (T data) { head = new node(data, head); }
+
+    public T peek() {
+        if (!isEmpty()) return head.data;
+        else throw new org.example.EmptyStackException("Стек не содержит элементов.");
+    }
+
+    public T pop () {
+        if (head != null) {
+            node tmpr = head;
+            head = head.next;
+            return tmpr.data;
+        }
+        else throw new EmptyStackException("Стек не содержит элементов.");
+    }
+}
+
